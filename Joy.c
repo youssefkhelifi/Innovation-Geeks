6b5502f1a4ec2fce121f3fb5c58fd8574c0ba9f8
JoyGiovanni <joyanaclet.bakonegiovanni@esprit.tn>
2021-05-06 14:32:13 -0700

    Mise a jour de fonctions joy

--- Joy.c
+++ Joy.c
@@ -1,46 +1,124 @@
+/** 
+* @file fonction.c 
+* @brief Fonctions 
+* @author Joy Giovanni 
+* @version 1.0 
+* @date Mai 01, 2021 
+* 
+* Testing program for Minimap Temp CollisionPP * 
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
-#include <SDL/SDL_ttf.h>
 #include <SDL/SDL_mixer.h>
+#include <SDL/SDL_ttf.h>
 #include "fonction.h"
 
-SDL_Rect MAJMinimap(SDL_Rect posJouer, int redimensionnement)
-{
-Personne per;
-
-posJouer.x=per.pos.x*(redimensionnement/100);
-posJouer.y=per.pos.y*(redimensionnement/100);
-
-return per.pos;
-}
+/** 
+* @brief Pour initialiser la Minimap . 
+* @param m la minimap  
+* @return rien 
+*/ 
 
 void initmap(minimap *m)
 {
-Personne per;
- m->image_miniature=IMG_Load("minibackground.bmp");
-
- m->position_minimap.x =300;
- m->position_minimap.y =0;
-
- m->image_bonhomme=IMG_Load("miniimge.bmp");
- m->position_bonhomme.x =300;
- m->position_bonhomme.y =100;
+    m->position_mini.x = 0;
+    m->position_mini.y = 0;
+    m->sprite = NULL;
+    m->sprite = IMG_Load("minimap1.png");
 }
+
+/** 
+* @brief Pour afficher la minimap  . 
+* @param m la minimap 
+* @param screen le background  
+* @return rien 
+*/ 
 
 void afficherminimap(minimap m, SDL_Surface *screen)
 {
-SDL_BlitSurface(m.image_miniature,NULL,screen,&m.position_minimap);
-SDL_BlitSurface(m.image_bonhomme,NULL,screen,&m.position_bonhomme);
+
+    SDL_BlitSurface(m.sprite, NULL, screen, &m.position_mini);
 }
 
-void liberer (minimap *m)
+/** 
+* @brief liberer la minmap . 
+* @param m pour la minimap   
+* @return rien
+*/ 
+
+void free_minimap(minimap *m)
 {
-    SDL_Flip(m->image_bonhomme);
-    SDL_Flip(m->image_miniature);
+    SDL_FreeSurface(m->sprite);
 }
 
+/** 
+* @brief Initialiser le background. 
+* @param tpour le temps  
+* @return rien 
+*/ 
+
+void initialiser_temps(temps *t)
+{
+    t->texte = NULL;
+    t->position.x = 0;
+    t->position.y = 100;
+    t->police = NULL;
+    t->police = TTF_OpenFont("avocado.ttf", 40);
+    strcpy(t->entree, "");
+    (t->secondesEcoulees) = 0;
+    time(&(t->t1)); //temps du debut
+}
+
+/** 
+* @brief afficher le temps. 
+* @param t pour le temps
+* @param screen pour le background  
+* @return rien
+*/ 
+
+void afficher_temps(temps *t, SDL_Surface *screen)
+{
+    SDL_Color couleurnoir = {0, 0, 0};
+    
+    time(&(t->t2)); // temps actuel
+
+    t->secondesEcoulees = t->t2 - t->t1;
+
+    t->min = ((t->secondesEcoulees / 60) % 60);
+    t->sec = ((t->secondesEcoulees) % 60);
+    int millisecondes=SDL_GetTicks()%60;
+    
+    sprintf(t->entree, "%02d:%02d:%02d", t->min, t->sec, millisecondes);
+
+    t->texte = TTF_RenderText_Blended(t->police, t->entree, couleurnoir);
+
+    SDL_BlitSurface(t->texte, NULL, screen, &(t->position)); /* Blit du texte */
+}
+
+/** 
+* @brief liberer le temps . 
+* @param t pour le temps
+* @param screen pour le background 
+* @return rien
+*/ 
+
+void free_temps(temps *t, SDL_Surface *screen)
+{
+    SDL_FreeSurface(t->texte);
+    TTF_CloseFont(t->police);
+}
+
+/** 
+* @brief To initialize les couleurs . 
+* @param background pour le background 
+* @param x pour la position de l'abcisse 
+* @param y pour la position de l'ordonnee 
+* @return rien
+*/ 
 SDL_Color GetPixel(SDL_Surface *Background, int x, int y)
 {
 
@@ -49,58 +127,78 @@ SDL_Color GetPixel(SDL_Surface *Background, int x, int y)
     //Determine position
 
     char *pixelPosition = (char *)Background->pixels;
-    //Offset by Y
+    
     pixelPosition += (Background->pitch * y);
-    //Offset by X
+    
     pixelPosition += (Background->format->BytesPerPixel * x);
-    //Copy pixel data
+    
     memcpy(&col, pixelPosition, Background->format->BytesPerPixel);
-    //Convert to color
+    
     SDL_GetRGB(col, Background->format, &color.r, &color.g, &color.b);
 
     return (color);
 }
 
-int collisionPP(Personne p, SDL_Surface * backgroundMasque)
+/** 
+* @brief verifier la collision . 
+* @param p pour le personnage 
+* @param Masque pour le background masque
+* @return rien 
+*/ 
+int collisionPP(Personne p, SDL_Surface *Masque)
 {
-    int collision = 0, i;
-    SDL_Color couleur_obstacle = {0,0,0};
-    SDL_Color couleur;
-    SDL_Rect pos [8];
+    SDL_Color test, couleurnoir = {0, 0, 0};
 
-    pos[0].x = p.pos.x;
-    pos[0].y = p.pos.y;
+    SDL_Rect pos[8];
+    pos[0].x = p.position_perso.x;
+    pos[0].y = p.position_perso.y;
+    pos[1].x = p.position_perso.x + p.position_perso.w / 2;
+    pos[1].y = p.position_perso.y;
+    pos[2].x = p.position_perso.x + p.position_perso.w;
+    pos[2].y = p.position_perso.y;
+    pos[3].x = p.position_perso.x;
+    pos[3].y = p.position_perso.y + p.position_perso.h / 2;
+    pos[4].x = p.position_perso.x;
+    pos[4].y = p.position_perso.y + p.position_perso.h;
+    pos[5].x = p.position_perso.x + p.position_perso.w / 2;
+    pos[5].y = p.position_perso.y + p.position_perso.h;
+    pos[6].x = p.position_perso.x + p.position_perso.w;
+    pos[6].y = p.position_perso.y + p.position_perso.h;
+    pos[7].x = p.position_perso.x + p.position_perso.w;
+    pos[7].y = p.position_perso.y + p.position_perso.h / 2;
+    int collision = 0, x, y;
 
-    pos[1].x = p.pos.x + (p.image->w)/2;
-    pos[1].y = p.pos.y;
-
-    pos[2].x = p.pos.x + p.image->w ;
-    pos[2].y = p.pos.y;
-
-    pos[3].x = p.pos.x ;
-    pos[3].y = p.pos.y + p.image->h ;
-
-    pos[4].x = p.pos.x ;
-    pos[4].y = p.pos.y +( p.image->h)/2;
-
-    pos[5].x = p.pos.x + (p.image->w)/2;
-    pos[5].y = p.pos.y + p.image->h;
-
-    pos[6].x = p.pos.x + p.image->w;
-    pos[6].y = p.pos.y + p.image->h;
-
-    pos[7].x = p.pos.x + p.image->w;
-    pos[7].y = p.pos.y + (p.image->h)/2;
-
-    for (i=0; i<8; i++)
+    for (int i = 0; i < 8 && collision == 0; i++)
     {
-
-        couleur=GetPixel(p.image, pos[i].x, pos[i].y);
-        if(couleur.b==0 && couleur.g==0 && couleur.r==0)
-        {
-            collision=1;
-            break;
-        }
+        x = pos[i].x;
+        y = pos[i].y;
+        test = GetPixel(Masque, x, y);
+        if (test.r == 0 && test.g == 0 && test.b == 0)
+            collision = 1;
     }
     return collision;
 }
+
+/** 
+* @brief redimensionner la position du personnage sur la minimap . 
+* @param p pour le personnage 
+* @param m pour la minimap 
+* @param camera pour la camera
+* @param redimensionnement pour redimensionner
+* @return rien 
+*/ 
+void majminimap (Personne *p, minimap *m ,SDL_Rect camera ,int redimensionnement){
+  if (camera.x==0)
+  p->position_perso.x += redimensionnement;
+
+  if (camera.x==1)
+  p->position_perso.x -= redimensionnement;
+
+  if (camera.x==2)
+  p->position_perso.y += redimensionnement;
+
+  if (camera.x==3)
+  p->position_perso.y -= redimensionnement;
+
+}
+
--- joy.h
+++ joy.h
@@ -1,27 +1,70 @@
+/** 
+* @file fonction.h 
+* @brief Les entete 
+* @author Joy Giovanni 
+* @version 1.0 
+* @date Mai 01, 2021 
+* 
+* Testing program for Minimap Temp CollisionPP * 
+*/
+
+
+#ifndef Karim_Akkari_1A30_H_
+#define Karim_Akkari_1A30_H_
+#include <time.h>
+
+/** 
+* @struc personne 
+* @brief struct pour les personnages 
+*/ 
+
 typedef struct
 {
-    SDL_Rect pos;
-    SDL_Surface * image;
+	SDL_Rect position_perso; /*!< position*/
+	SDL_Surface *sprite; /*!< image*/
 } Personne;
 
-typedef struct
-{
-    SDL_Surface * Surf;
-    SDL_Surface * surfM;
-} background;
+/** 
+* @struct minimap 
+* @brief structure pour la minimap
+*/ 
 
 typedef struct
 {
-    SDL_Surface* image_miniature;
-    SDL_Rect position_minimap;
-    SDL_Surface* image_bonhomme;
-    SDL_Rect position_bonhomme;
-}minimap;
+	SDL_Rect position_mini; /*!< Rectangle*/
+	SDL_Surface *sprite; /*!< image*/
+} minimap;
 
+/** 
+* @struct temps 
+* @brief structure pour le temps
+*/ 
 
-SDL_Color GetPixel(SDL_Surface *pSurface, int x, int y);
-int collisionPP( Personne p, SDL_Surface * backgroundMasque);
-SDL_Rect MAJMinimap(SDL_Rect posJouer, int redimensionnement);
+typedef struct temps
+{
+	SDL_Surface *texte; /*!< texte*/
+	SDL_Rect position; /*!< position*/
+
+	TTF_Font *police; /*!< police*/
+
+	char entree[100]; /*!<chaine de caractere*/
+	int secondesEcoulees; /*!< entier*/
+	SDL_Color couleurBlanche; /*!< couleur*/
+	time_t t1, t2; /*!< temps*/
+	int min, sec; /*!< entier*/
+} temps;
+
 void initmap(minimap *m);
 void afficherminimap(minimap m, SDL_Surface *screen);
-void liberer (minimap *m);
+void free_minimap(minimap *m);
+void majminimap (Personne *p, minimap *m ,SDL_Rect camera ,int redimensionnement);
+
+void initialiser_temps(temps *t);
+void afficher_temps(temps *t, SDL_Surface *ecran);
+void free_temps(temps *t, SDL_Surface *ecran);
+
+SDL_Color GetPixel(SDL_Surface *Background, int x, int y);
+int collisionPP(Personne p, SDL_Surface *Masque);
+
+#endif
+
